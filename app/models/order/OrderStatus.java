77ccb53cab433646e9f41d83ef852648aba2efa3
Shabna  Azeez <shabnaazeez92@gmail.com>
2018-03-12 11:20:38 +0530

    moved backend

--- app/models/order/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package models.order;
-
-/**
- * Created by muneeb on 17/03/17.
- */
-public enum OrderStatus {
-    CREATED,CONFIRMED,DELIVERED,CANCELED
-}
--- app/models/user/UserType.java
+++
@@ -1,8 +0,0 @@
-package models.user;
-
-/**
- * Created by muneeb on 06/02/17.
- */
-public enum UserType {
-    ADMIN,USER
-}
